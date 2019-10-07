6a69ac68136b94bd30b3d64dc27620456524cb4d
austin bittinger <sixteenbitt@gmail.com>
2019-10-06 23:43:18 -0500

    Add logfile path for pointing to PID 1's stdout

--- arguments.c
+++ arguments.c
@@ -1,23 +1,44 @@
 #include "string.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "stdbool.h"
 #include "kubemem.h"
+
+bool arg_next_bounds_check(char *flag_name, int argc, int idx) {
+  if(idx + 1 >= argc) {
+    fprintf(stderr, "Missing required parameter for %s\n", flag_name);
+    return false;
+  }
+
+  return true;
+}
 
 int kubemem_arguments(int argc, char **argv, struct kubemem_arguments *args) {
   args->warning_ratio = DEFAULT_WARNING_RATIO;
   args->failure_ratio = DEFAULT_FAILURE_RATIO;
+  args->logfile_path = DEFAULT_LOGFILE_PATH;
 
   for(int i = 1; i < argc; i++) {
     if(strcmp(argv[i], WARNING_FLAG) == 0) {
+      if(!arg_next_bounds_check(WARNING_FLAG, argc, i)) {
+	return ERR_ARGUMENTS_FAILED;
+      }
+
       args->warning_ratio = strtod(argv[++i], NULL);
-      if(args->warning_ratio != 0.0) {
-        args->warning_ratio /= 100.0;
-      }
+      args->warning_ratio /= 100.0;
     } else if(strcmp(argv[i], FAILURE_FLAG) == 0) {
-      args->failure_ratio = strtod(argv[++i], NULL);
-      if(args->failure_ratio != 0.0) {
-        args->failure_ratio /= 100.0;
+      if(!arg_next_bounds_check(FAILURE_FLAG, argc, i)) {
+	return ERR_ARGUMENTS_FAILED;
       }
+ 
+      args->failure_ratio = strtod(argv[++i], NULL);
+      args->failure_ratio /= 100.0;
+    } else if(strcmp(argv[i], LOGFILE_FLAG) == 0) {
+      if(!arg_next_bounds_check(LOGFILE_FLAG, argc, i)) {
+	return ERR_ARGUMENTS_FAILED;
+      }
+      
+      args->logfile_path = argv[++i];
     } else {
       fprintf(stderr, "Unrecognized argument: %s\n", argv[i]);
       return ERR_ARGUMENTS_FAILED;
@@ -26,3 +47,4 @@ int kubemem_arguments(int argc, char **argv, struct kubemem_arguments *args) {
 
   return 0;
 }
+
--- kubemem.h
+++ kubemem.h
@@ -5,16 +5,20 @@
 #define ERR_SYSINFO_FAILED 2
 #define ERR_ARGUMENTS_FAILED 2
 #define ERR_MEMORY_LOW 3
+#define ERR_LOGFILE_FAILED 4
 
 #define DEFAULT_WARNING_RATIO 0.75
 #define DEFAULT_FAILURE_RATIO 0.90
+#define DEFAULT_LOGFILE_PATH "/dev/stdout"
 
 #define WARNING_FLAG "--warning"
 #define FAILURE_FLAG "--failure"
+#define LOGFILE_FLAG "--logfile"
 
 struct kubemem_arguments {
   double warning_ratio;
   double failure_ratio;
+  char *logfile_path;
 };
 
 double free_ram_ratio();
